c7df1bd98a5979c46d998a1f23c1adfcc2e1a71c
Michael Watson <m.watson.tn@gmail.com>
2017-07-29 14:32:06 -0500

    * .gitignore: initial commit
    * README.md:
    * ComicBookGallery.sln:
    * ComicBookGallery.userprefs:
    * Web.config:
    * Global.asax:
    * Global.asax.cs:
    * packages.config:
    * Web.config:
    * packages.config:
    * ComicBookGallery.csproj:
    * RouteConfig.cs:
    * Test.cs:
    * ComicBookGallery.Tests.csproj:

---
+++ ComicBookGallery.Tests/Controllers/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ComicBookGallery.Tests.Controllers
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
